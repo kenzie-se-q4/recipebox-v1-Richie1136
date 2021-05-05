d1bfee1a09bf5bdb590b4b31c8b4842420742f75
Richie1136 <Richiemajor1@gmail.com>
2021-05-05 13:38:20 -0400

    Finished creating my models and set up the migrations folder which has the model information inside of it

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Author(models.Model):
-  name = models.CharField(max_length = 50)
+  name = models.CharField(max_length=50)
   bio = models.TextField()
 
 
 class Recipe(models.Model):
-  title = models.CharField(max_length = 30)
-  author = foreignkey
+  title = models.CharField(max_length=30)
   description = models.TextField()
-  time_required = models.CharField(max_length = 200)
+  time_required = models.CharField(max_length=40)
   instructions = models.TextField()
+  author = models.ForeignKey(Author, on_delete=models.CASCADE)
